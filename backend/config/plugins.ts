cbfe7ca882a46c8014220402467f54dd8081e1fa
Nihad Amin <nihad.amin@nordakademie.de>
2023-01-26 18:30:52 +0100

    fixed email configuration

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,10 +1,20 @@
 module.exports = ({ env }) => ({
   email: {
-    provider: 'nodemailer',
-    providerOptions: {
-      host: 'localhost',
-      port: 1025,
-      ignoreTLS: true,
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: env('SMTP_FROM'),
+        defaultReplyTo: env('SMTP_FROM'),
+      },
     },
   },
 });
